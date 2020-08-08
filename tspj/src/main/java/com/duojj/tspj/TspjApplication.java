1408309b22bbd12e2803b91d975e25f5ec9cbac3
Jaehyunleejava <45728768+Jaehyunleejava@users.noreply.github.com>
2020-08-08 19:17:00 +0900

    DB Mybatis Setting

---
+++ tspj/src/main/java/com/duojj/tspj/TspjApplication.java
@@ -0,0 +1,13 @@
+package com.duojj.tspj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TspjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TspjApplication.class, args);
+    }
+
+}
---
+++ tspj/src/test/java/com/duojj/tspj/TspjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duojj.tspj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TspjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
